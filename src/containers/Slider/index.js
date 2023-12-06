c82832025340003cb9f6050ef6b583f126874c34
jim294 <jeanmariedeschamps25@gmail.com>
2023-12-06 21:15:17 +0100

    scrolling radio buttons

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -40,9 +40,9 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((evt, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${evt.id}`}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
